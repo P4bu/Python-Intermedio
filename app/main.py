3602f2fa1f2654249588f62f978f8519256ee49f
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2024-10-16 15:34:18 -0300

    Fin: termino de modulo de paquetes y modulos con python

--- app/main.py
+++ app/main.py
@@ -1,19 +1,23 @@
 import utils
-keys, values = utils.get_ppopulation()
-print(keys, values)
-
 
 data = [
-    {
-        'country': 'Chile',
-        'population': 500
-    },
-    {
-        'country': 'Colombia',
-        'population': 700
-    }
-]
-country = input('Ingresa el Pais: ')
+        {
+            'country': 'Chile',
+            'population': 500
+        },
+        {
+            'country': 'Colombia',
+            'population': 700
+        }
+    ]
+def run():
+    keys, values = utils.get_ppopulation()
+    print(keys, values)
 
-result = utils.population_by_country(data, country)
-print(result)
+    country = input('Ingresa el Pais: ')
+
+    result = utils.population_by_country(data, country)
+    print(result)
+
+if __name__=='__main__':
+    run()
---
+++ pkg/__init__.py
@@ -0,0 +1,5 @@
+print('se inicio el paquete')
+
+URL = 'Platzi.com'
+
+import pkg.mod_1, pkg.mod_2
