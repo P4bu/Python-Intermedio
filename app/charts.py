c78b84f1012ee6f86bc2904d9b0935db1643c30c
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2024-10-17 14:11:32 -0300

    creacion de graficos con matplotlib

---
+++ app/charts.py
@@ -0,0 +1,17 @@
+# %%
+# import matplotlib.pyplot as plt
+# import numpy as np
+
+# %%
+def generate_bar_chart():
+
+
+    fig, ax = plt.subplots()
+    ax.bar(label, values)
+    plt.show()
+    
+
+if __name__ == '__main__':
+    label = ['a', 'b', 'c']
+    values = [100,200,80]
+    generate_bar_chart()
