9a69afc6c9a1dbc0d1d0fc80e0f4e618d97f435d
Pablo Barra Urtubia <pbarrau.90@icloud.com>
2024-10-17 14:13:05 -0300

    comentari a codigo

--- app/charts.py
+++ app/charts.py
@@ -1,3 +1,4 @@
+"""
 # %%
 # import matplotlib.pyplot as plt
 # import numpy as np
@@ -15,3 +16,5 @@ if __name__ == '__main__':
     label = ['a', 'b', 'c']
     values = [100,200,80]
     generate_bar_chart()
+
+"""
